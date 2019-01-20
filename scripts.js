3d7fb671cca29ab927f65fda0ebcd0404ce3bf6e
Anna Malecki <annarosemalecki@gmail.com>
2019-01-20 15:59:52 -0500

    Added API call with simple text display
    Successfully calls datamuse API with a word from user input and displays text results of synonym.

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function getSynonyms(){
+    document.getElementById("resultsArea").innerHTML = "";
+    var request = new XMLHttpRequest();
+    var searchInput = document.getElementById("searchInput").value;
+    var url = "https://api.datamuse.com/words?rel_syn="+searchInput;
+
+    request.open('GET', url, true);
+    request.onload = function () {
+
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400) {
+        //console.log(result.word);
+        data.forEach(function(element) {
+            document.getElementById("resultsArea").innerHTML += element.word + " ";
+        });
+        
+    } 
+    else {
+        console.log('error');
+    }
+    }
+
+    request.send();    
+    
+}
