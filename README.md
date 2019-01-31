a82fd89ba847b6ff07dc4155818fbcd056462123
Anna Malecki <annarosemalecki@gmail.com>
2019-01-31 14:50:15 -0500

    added comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab1
+
+Synonym Rolls is a website to help writers find the right word. Enter a word in the search bar and view synonyms for that word and their definitions. 
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Function that calls the DataMuse API to retrieve synonyms for the users search query. 
+//DataMuse API: https://www.datamuse.com/api/ 
 function getSynonyms() {
     document.getElementById("resultsArea").innerHTML = "";
     var request = new XMLHttpRequest();
@@ -20,12 +22,14 @@ function getSynonyms() {
 
         //Handle JSON response
         var data = JSON.parse(this.response);
-
+        
+        //If no synonyms are found display a message
         if (request.status >= 200 && request.status < 400) {
-            //console.log(result.word);
+            if(data.length == 0){
+               document.getElementById("resultsArea").innerHTML += "We're speechless... No synonyms found.";
+            }
+            //Go through all results of response and create a card for each synonym with definition
             data.forEach(function (element) {
-                //document.getElementById("resultsArea").innerHTML += element.word + " ";
-                
                 const card = document.createElement('div');
                 card.setAttribute('class','card');
                 const title = document.createElement('h1');
@@ -40,13 +44,14 @@ function getSynonyms() {
             });
 
         } else {
-            console.log('error');
+            console.log('Error from datamuse API');
         }
     }
 
     request.send();
 }
 
+//function to call Wordnik API to get definition of words
 function getDefinition(word,p){
     var request2 = new XMLHttpRequest();
     var url_wordnik = "https://api.wordnik.com/v4/word.json/" + word + "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=6154a97e2c3f02c2541080c31690924ea7cd4fa3ac7e47476";
@@ -62,7 +67,7 @@ function getDefinition(word,p){
             definition = data[0].text;
             p.textContent = definition;
         } else {
-            console.log('Error getting definition');
+            console.log('Error from wordnik API');
         }
     }
     request2.send();
